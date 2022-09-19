4534d037132db50d089e6374ff1350c70d46788b
Keerthi_Uppada <69393162+lisa2127@users.noreply.github.com>
2022-09-19 15:16:59 +0530

    Time: 75 ms (27.34%), Space: 12.4 MB (55.02%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void markconnected(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]!='1'){
+            return;
+        }
+        grid[i][j]='2';
+        markconnected(grid,i-1,j);
+        markconnected(grid,i+1,j);
+        markconnected(grid,i,j-1);
+        markconnected(grid,i,j+1);     
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int islands=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    markconnected(grid,i,j);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+};

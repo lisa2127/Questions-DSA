9e6f9fa4e3aef7f69aaf74b2d1f0c341f093bb46
Keerthi_Uppada <69393162+lisa2127@users.noreply.github.com>
2022-09-25 14:50:49 +0530

    Time: 88 ms (41.67%), Space: 23.2 MB (83.33%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
 
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<string> ans;
+        //Approach 1
+        /*vector<string> ans;
         unordered_map<int,string> mp;
         for(int i=0;i<heights.size();i++){
             mp[heights[i]]=names[i];
@@ -10,6 +11,19 @@ public:
         sort(heights.begin(),heights.end());
         for(int i=heights.size()-1;i>=0;i--){
             ans.push_back(mp[heights[i]]);
+        }
+        return ans;*/
+        
+        //Approach -2
+        
+        priority_queue<pair<int,string>> pq;
+        for(int i=0;i<names.size();i++){
+            pq.push({heights[i],names[i]});
+        }
+        vector<string> ans;
+        while(pq.size()>0){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }

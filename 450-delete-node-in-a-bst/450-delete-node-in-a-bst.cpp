7175fd2703c9a1f335e49b72db116568143405e8
Keerthi_Uppada <69393162+lisa2127@users.noreply.github.com>
2022-10-05 10:25:14 +0530

    Time: 63 ms (46.96%), Space: 32.7 MB (41.45%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* minval(TreeNode* root){
+        TreeNode* temp=root;
+        while(temp->left!=NULL){
+            temp=temp->left;
+        }
+        return temp;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL){
+            return root;
+        }
+        
+        if(root->val==key){
+            // 0->children
+            if(root->left==NULL && root->right==NULL){
+                delete root;
+                return NULL;
+            }
+            
+            //1-> child
+            if(root->left!=NULL && root->right==NULL){
+                TreeNode* temp=root->left;
+                delete root;
+                return temp;
+            }
+            if(root->left==NULL && root->right!=NULL){
+                TreeNode* temp=root->right;
+                delete root;
+                return temp;
+            }
+            
+            //2->children
+            if(root->left!=NULL && root->right!=NULL){
+                int mini=minval(root->right)->val;
+                root->val=mini;
+                root->right=deleteNode(root->right,mini);
+                return root;
+            }
+        }
+        if(root->val <key){
+            root->right=deleteNode(root->right,key);
+            return root;
+        }
+        else{
+            root->left=deleteNode(root->left,key);
+            return root;
+        }
+    }
+};

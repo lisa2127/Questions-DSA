03c0db4dd521a383a50614186bc04b5b9eee2b26
Keerthi_Uppada <69393162+lisa2127@users.noreply.github.com>
2022-10-07 10:50:46 +0530

    Time: 14 ms (26.67%), Space: 11.7 MB (15.25%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,13 +1,47 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        vector<int> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                ans.push_back(i);
+    int firstOccurence(vector<int>& nums, int target){
+        int start = 0;
+        int res = -1;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target==nums[mid]){
+                res = mid;
+                end = mid-1;
             }
+            else if(target<nums[mid])
+                end = mid-1;
+            else 
+                start = mid+1;
         }
-        return ans;
+        return res;
+    }
+    int lastOccurence(vector<int>& nums, int target){
+        int start = 0;
+        int res = -1;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target==nums[mid]){
+                res = mid;
+                start = mid+1;
+            }
+            else if(target<nums[mid])
+                end = mid-1;
+            else 
+                start = mid+1;
+        }
+        return res;
+    }
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> result;
+        sort(nums.begin(), nums.end());
+        int first = firstOccurence(nums, target);
+        int last = lastOccurence(nums, target);
+        if(first!=-1 && last!=-1){
+        for(int i=first; i<=last; i++)
+            result.push_back(i);}
+        return result;
     }
 };
